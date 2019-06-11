69e4eb3f3ee924f0321c0712842f5a424404b624
godlikea <1114151497@qq.com>
2019-06-11 09:21:30 +0800

    131

--- src/test/java/com/kai/user/StreamTest.java
+++ src/test/java/com/kai/user/StreamTest.java
@@ -1,9 +1,15 @@
 package com.kai.user;
 
+import org.junit.Test;
+
 /**
  * @author ggk
  * @date 2019/6/11 0011 上午 9:00
  */
 public class StreamTest {
 
+    @Test
+    public void streamTest(){
+
+    }
 }
